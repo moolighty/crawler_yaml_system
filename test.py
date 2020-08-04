123f3502ca34e2d8b8c866e8578348daa4463b1b
liangchuanjian <liangchuanjian@bytedance.com>
2020-08-04 23:21:47 +0800

    fix: update pyyaml
    Change-Id: Id31ceb8cdcadd60c73a2e5ad99212ff7d1412b19

--- test.py
+++ test.py
@@ -4,6 +4,6 @@
 from src.run_spider import run
 
 if __name__ == '__main__':
-    run('example/example_multi.91wenmi.com', "dev")
-    # run('example/example_xml', "dev")
+    # run('example/example_multi.91wenmi.com', "dev")
+    run('example/example_xml', "dev")
     # run('example/example_json_xml', "dev")
